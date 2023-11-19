6679e7e806adb94b5e8e264c9de790c9b92b2d10
Your Name <you@example.com>
2023-11-19 20:55:52 +0700

    React-hook

--- src/components/AddUserInfo.js
+++ src/components/AddUserInfo.js
@@ -1,54 +1,46 @@
 import React from "react";
+import { useState } from "react";
 
-class AddUserInfor extends React.Component{
-    state = {
-        name:'hieu san',
-        address:"google",
-        age:25
+const AddUserInfor = (props) =>{
+    const [name, setName] = useState("hieu san");
+    const [age, setAge] = useState(25);
+
+    const handleOnChangeAge=(event)=>{
+        setAge(event.target.value)
     }
 
-   
-    handleOnChangeAge=(event)=>{
-        this.setState({
-            age:event.target.value
-        })
-    }
-    handleOnChangeInput=(event)=>{
-        this.setState({
-            name: event.target.value
-        })
+    const handleOnChangeInput=(event)=>{
+        setName(event.target.value);
 
     }
-    handleOnSubmit =(event)=>{
+    const handleOnSubmit =(event)=>{
         event.preventDefault();
         const randomNumber = Math.random() * 100;
-        this.props.handleAddNewUser({
+        props.handleAddNewUser({
             id:Math.ceil(randomNumber),
-            name: this.state.name,
-            age: this.state.age
+            name: name,
+            age: age
         });
     }
-    
-    render(){
 
-        
-        return(
-            <div>
-                {<p>Toi ten {this.state.name} nam nay toi {this.state.age} tuoi </p>}
-                <form onSubmit={(event)=>{
-                    this.handleOnSubmit(event)
-                }}>
-                <label> Your name:</label>
-<input type="text" value={this.state.name} onChange={(event)=> this.handleOnChangeInput(event)}/>
+    return(
+        <div>
+            {<p>Toi ten {name} nam nay toi {age} tuoi </p>}
+            <form onSubmit={(event)=>{
+                handleOnSubmit(event)
+            }}>
+            <label> Your name:</label>
+<input type="text" value={name} onChange={(event)=> handleOnChangeInput(event)}/>
 
 <label> Your Age:</label>
 
-                <input type="text" value={this.state.age} onChange={(event)=> this.handleOnChangeAge(event)}/>
-                <button >Submit</button>
-                </form>
-            </div>
-        )
-    }
+            <input type="text" value={age} onChange={(event)=> handleOnChangeAge(event)}/>
+            <button >Submit</button>
+            </form>
+        </div>
+    )
+
 }
+
 
 export default AddUserInfor
--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,39 +1,39 @@
+
 import React from "react";
 import "./Displayinfor.scss"
-class DisplayInfo extends React.Component{
-    state = {
-        isShowListUser :true
-    }
-    handleShowHide=(event)=>{
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
+import { useState } from "react";
+const DisplayInfo = (props) =>{
+
+    const [isShowListUser, setIsListUser] = useState(true);
+
+   const handleShowHide=(event)=>{
+    console.log(props.listUser);
+
+        setIsListUser(!isShowListUser);
        
     }
-    render(){
-    const {listUser} = this.props;
-        return(
-            <div className="display-infor-container">
-                <div >
-                    <p onClick={()=>{
-                        this.handleShowHide()
-                    }}> {this.state.isShowListUser === true? "Hide list user" : "Show list user"} </p>
-                </div>
-                {this.state.isShowListUser && 
-                <div>
-                   { listUser.map((item)=>{
-                    return(
-                        <div className={+item.age > 20 ? "green" : "red"} key={item.id}>
 
-                                    <p>ten toi la: {item.name}</p>
-                                    <p>tuoi toi la: {item.age}</p>
-                                </div>
-                    )
-                    })}
-                </div>}
+    return(
+        <div className="display-infor-container">
+            <div >
+                <p onClick={()=>{
+                    handleShowHide()
+                }}> {isShowListUser === true? "Hide list user" : "Show list user"} </p>
             </div>
-        )
-    }
+            {isShowListUser && 
+            <div>
+               { props.listUser.map((item)=>{
+                return(
+                    <div className={+item.age > 20 ? "green" : "red"} key={item.id}>
+
+                                <p>ten toi la: {item.name}</p>
+                                <p>tuoi toi la: {item.age}</p>
+                            </div>
+                )
+                })}
+            </div>}
+        </div>
+    )
 }
 
 export default DisplayInfo;
